3efccd9bfb897e9e51e99368c37f5f7c93ea5dd2
Jason Vann <vannjt1@icloud.com>
2021-12-05 17:16:23 -0500

    post, put, and delete complete

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -15,22 +15,17 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:id', md.checkAccountId, async (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    const account = await Account.getById(req.params.id)
-    res.json(account)
-  } catch (err) {
-    next(err)
-  }
+  res.json(req.account)
 })
 
 router.post('/', 
   md.checkAccountPayload, 
   md.checkAccountNameUnique, 
-  (req, res, next) => {
+  async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('post account')
+    const newAccount = await Account.create(req.body)
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
@@ -40,8 +35,9 @@ router.put('/:id',
   md.checkAccountId,
   md.checkAccountPayload,
   md.checkAccountNameUnique,
-  (req, res, next) => {
-  // DO YOUR MAGIC
+  async (req, res, next) => {
+  const updated = await Account.updateById(req.params.id, req.body)
+  res.json(updated)
   try {
     res.json('update account')
   } catch (err) {
@@ -49,10 +45,11 @@ router.put('/:id',
   }
 });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('delete account')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
